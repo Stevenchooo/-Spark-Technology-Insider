d096e8d0e79e3f12b62620de5595a00beeaba5a5
Steven <cszcszohye@qq.com>
2017-10-23 09:38:11 +0800

    Update

--- src/java/FunctionJava.java
+++ src/java/FunctionJava.java
@@ -1,8 +1,13 @@
 /**
- * Created by cWX491729 on 2017/9/21.
+ * Created by c00491729 on 2017/9/21.
  */
 public class FunctionJava {
     public static void main(String[] args) {
+        String a = "xxx";
+        String b = "yyy";
+        String c = a + b ;
+        System.out.println(c);
+
 
     }
 }
